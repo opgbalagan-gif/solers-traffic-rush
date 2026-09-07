e988cbd01de2082cc6625b75c70f376d4f762a12
opgbalagan-gif <opgbalagan-gif@users.noreply.github.com>
2026-09-07 17:14:17 +0300

    Connect verified public Google Sheets leaderboard

--- src/config/leaderboard.ts
+++ src/config/leaderboard.ts
@@ -1,2 +1,2 @@
 // Public Apps Script deployment URL; no passwords or server signing keys belong here.
-export const GOOGLE_SHEETS_API = '';
+export const GOOGLE_SHEETS_API = 'https://script.google.com/macros/s/AKfycbyCcXRUgh1DKEUjoNRJrtdAyuY8hMfE7y5l4k2Yqb7qTexvAc6HnRq5h-RP9B9UQWomMw/exec';
